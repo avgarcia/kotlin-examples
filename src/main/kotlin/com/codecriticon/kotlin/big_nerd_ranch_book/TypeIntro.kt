cf42a85994a2038e7db4eaa5a27383028c45cdd5
Antonio Vidal <avidal@stratio.com>
2019-09-19 09:35:14 +0200

    Chapter 2

---
+++ src/main/kotlin/com/codecriticon/kotlin/big_nerd_ranch_book/TypeIntro.kt
@@ -0,0 +1,12 @@
+package com.codecriticon.kotlin.big_nerd_ranch_book
+
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main() {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    experiencePoints += 5
+
+    println(playerName)
+    println(experiencePoints)
+}
