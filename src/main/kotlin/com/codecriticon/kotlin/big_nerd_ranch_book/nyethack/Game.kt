5436c58259812d93cb18548c90d7d028625dd836
Antonio Vidal <avidal@stratio.com>
2019-09-20 10:45:16 +0200

    Chapter 3

---
+++ src/main/kotlin/com/codecriticon/kotlin/big_nerd_ranch_book/nyethack/Game.kt
@@ -0,0 +1,36 @@
+package com.codecriticon.kotlin.big_nerd_ranch_book.nyethack
+
+fun main() {
+    val name = "Madrigal"
+    val healthPoints = 100
+    val isBlessed = true
+    val isInmortal = true
+
+    val race = "gnome"
+    val faction = when (race) {
+        "dwarf" -> "Keepers of the Mines"
+        "gnome" -> "Keepers of the Mines"
+        "orc" -> "Free People of the Rolling Hills"
+        "human" -> "Free People of the Rolling Hills"
+        else -> "Nothing"
+    }
+
+    val auraVisible = isBlessed && healthPoints > 50 || isInmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+
+    val healthStatus = when(healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 ->  "has a few scratches."
+        in 75..89 -> {
+            if (isBlessed)
+                "has some minor wounds but is healing quite quickly!"
+            else
+                "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+
+    println("(Aura: $auraColor) (Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
