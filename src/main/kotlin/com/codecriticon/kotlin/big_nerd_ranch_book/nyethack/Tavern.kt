3b041dd69cd36e77bbe7b0ca01842cfd08e91cf2
Antonio Vidal <avidal@stratio.com>
2019-10-14 13:08:09 +0200

    Kotlin Chapter 7

--- src/main/kotlin/com/codecriticon/kotlin/big_nerd_ranch_book/nyethack/Tavern.kt
+++ src/main/kotlin/com/codecriticon/kotlin/big_nerd_ranch_book/nyethack/Tavern.kt
@@ -1,9 +1,37 @@
 package com.codecriticon.kotlin.big_nerd_ranch_book.nyethack
 
-fun main() {
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) it.capitalize() else "Buttered Ale"
-    }
+const val TAVERN_NAME = "Taernyl's Folly"
 
-    println(beverage)
+fun main() {
+    placeOrder("shandy,Dragon's Breath,5.91")
 }
+
+fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val phrase = if (name == "Dragron's Breath") {
+        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!")}"
+    } else {
+        "Madrigal says: Thanks for the $name."
+    }
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+}
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
