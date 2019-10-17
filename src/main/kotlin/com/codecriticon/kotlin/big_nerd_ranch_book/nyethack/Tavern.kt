895b8a2ed5b136a6ebc7e406c50595fdd7bcf09c
Antonio Vidal <avidal@stratio.com>
2019-10-17 10:44:32 +0200

    Chapter 11

--- src/main/kotlin/com/codecriticon/kotlin/big_nerd_ranch_book/nyethack/Tavern.kt
+++ src/main/kotlin/com/codecriticon/kotlin/big_nerd_ranch_book/nyethack/Tavern.kt
@@ -1,54 +1,69 @@
 package com.codecriticon.kotlin.big_nerd_ranch_book.nyethack
 
-import kotlin.math.roundToInt
+import java.io.File
 
 const val TAVERN_NAME = "Taernyl's Folly"
 
-var playerGold = 10
-var playerSilver = 10
+val patronList = mutableListOf("Eli", "Mordoc", "Spohie")
+val lastName = listOf("Ironfoot", "Fernsworth", "Baggins")
+val uniquePatrons = mutableSetOf<String>()
+val menuList = File("data/tavern-menu-data.txt").readText().split("\n")
+val patronGold = mutableMapOf<String, Double>()
 
 fun main() {
-    placeOrder("shandy,Dragon's Breath,5.91")
+    if (patronList.contains("Eli")) {
+        println("The tavern master says: Eli's in the back playing cards.")
+    } else {
+        println("The tavern master says: Eli isn't here.")
+    }
+
+    if (patronList.containsAll(listOf("Sophie", "Mordoc"))) {
+        println("The tavern master says: Yea, they're seated by the stew kettle.")
+    } else {
+        println("The tavern master says: Nay, they departed hours ago.")
+    }
+
+    (0..9).forEach {
+        val first = patronList.random()
+        val last = lastName.random()
+        val name = "$first $last"
+        uniquePatrons += name
+    }
+    uniquePatrons.forEach {
+        patronGold[it] = 6.0
+    }
+
+    var orderCount = 0
+    while (orderCount <= 9) {
+        placeOrder(uniquePatrons.random(), menuList.random())
+        orderCount++
+    }
+
+    displayPatronBalances()
 }
 
-fun placeOrder(menuData: String) {
+fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
 
-    println("Madrigal speaks with $tavernMaster about their order.")
+    println("$patronName speaks with $tavernMaster about their order.")
 
     val (type, name, price) = menuData.split(',')
-    val message = "Madrigal buys a $name ($type) for $price."
+    val message = "$patronName buys a $name ($type) for $price."
     println(message)
-
-    performPurchase(price.toDouble())
+    performPurchase(price.toDouble(), patronName)
 
     val phrase = if (name == "Dragron's Breath") {
-        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!")}"
+        "$patronName exclaims ${toDragonSpeak("Ah, delicious $name!")}"
     } else {
-        "Madrigal says: Thanks for the $name."
+        "$patronName says: Thanks for the $name."
     }
-    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+    println("$patronName exclaims: ${toDragonSpeak(phrase)}")
 }
 
-fun performPurchase(price: Double) {
-    displayBalance()
-    val totalPurse = playerGold + (playerSilver / 100.0)
-    println("Total purse: $totalPurse")
-    println("Purchasing item for $price")
-
-    val remainingBalance = totalPurse - price
-    println("Remaining balance: ${"%.2f".format(remainingBalance)}")
-
-    val remainingGold = remainingBalance.toInt()
-    val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
-    playerGold = remainingGold
-    playerSilver = remainingSilver
-    displayBalance()
-}
-
-private fun displayBalance() {
-    println("Player's purse balance: Gold: $playerGold , Silver: $playerSilver")
+fun performPurchase(price: Double, patronName: String) {
+    val totalPurse = patronGold.getValue(patronName)
+    patronGold[patronName] = totalPurse - price
 }
 
 private fun toDragonSpeak(phrase: String) =
@@ -62,3 +77,9 @@ private fun toDragonSpeak(phrase: String) =
             else -> it.value
         }
     }
+
+private fun displayPatronBalances() {
+    patronGold.forEach { patron, balance ->
+        println("$patron, balance: ${"%.2f".format(balance)}")
+    }
+}
